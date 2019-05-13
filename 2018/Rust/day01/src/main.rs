56d2fd6b810da0b1666badc54f42c3574af4bcb1
drewsteinacher <26029945+drewsteinacher@users.noreply.github.com>
2019-05-13 14:49:48 -0400

    Implement Part 2 for finding the first repeated frequency

--- 2018/Rust/day01/src/main.rs
+++ 2018/Rust/day01/src/main.rs
@@ -2,6 +2,7 @@ use std::fs;
 use std::io;
 use std::num;
 use std::io::Read;
+use std::collections::HashSet;
 
 #[derive(Debug)]
 enum CliError {
@@ -28,6 +29,9 @@ fn main() -> Result<(), CliError> {
     let resulting_frequency = resulting_frequency(&contents)?;
     println!("Part 1: Resulting frequency: {}", resulting_frequency);
     
+    let first_repeat = first_repeated_frequency(&contents)?;
+    println!("Part 2: First repeated frequency: {}", first_repeat);
+    
     Ok(())
 }
 
@@ -46,9 +50,28 @@ fn resulting_frequency(input: &str) -> Result<i32, CliError> {
     Ok(sum)
 }
 
+fn first_repeated_frequency(input: &str) -> Result<i32, CliError> {
+    
+    let mut sum = 0;
+    let mut observed_frequencies: HashSet<i32> = vec!(sum).into_iter().collect();
+    
+    loop {
+        for line in input.lines() {
+            
+            let change : i32 = line.trim().parse()?;
+            sum += change;
+            
+            let is_new = !observed_frequencies.insert(sum);
+            if is_new {
+                return Ok(sum);
+            }
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use super::resulting_frequency;
+    use super::{resulting_frequency, first_repeated_frequency};
     
     #[test]
     fn test_resulting_frequency() {
@@ -56,6 +79,12 @@ mod tests {
         assert_eq!(resulting_frequency("+1\n+1\n-2").unwrap(), 0);
         assert_eq!(resulting_frequency("-1\n-2\n-3").unwrap(), -6);
     }
-}
-
+    
+    #[test]
+    fn test_first_repeated_frequency() {
+        assert_eq!(first_repeated_frequency("+1\n-1").unwrap(), 0);
+        assert_eq!(first_repeated_frequency("+3\n+3\n+4\n-2\n-4").unwrap(), 10);
+        assert_eq!(first_repeated_frequency("-6\n+3\n+8\n+5\n-6").unwrap(), 5);
+        assert_eq!(first_repeated_frequency("+7\n+7\n-2\n-7\n-4").unwrap(), 14);
+    }
 }
