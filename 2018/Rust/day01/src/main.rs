a92401196a641ac47393a704c5f3aad08279537f
drewsteinacher <26029945+drewsteinacher@users.noreply.github.com>
2019-05-13 13:29:15 -0400

    First implementation for Part 1

--- 2018/Rust/day01/src/main.rs
+++ 2018/Rust/day01/src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use std::io;
+use std::num;
+use std::io::Read;
+
+#[derive(Debug)]
+enum CliError {
+    IoError(io::Error),
+    ParseError(num::ParseIntError)
+}
+
+impl From<io::Error> for CliError {
+    fn from(error: io::Error) -> Self {
+        CliError::IoError(error)
+    }
+}
+
+impl From<num::ParseIntError> for CliError {
+    fn from(error: num::ParseIntError) -> Self{
+        CliError::ParseError(error)
+    }
+}
+
+fn main() -> Result<(), CliError> {
+    
+    let path = "input.txt";
+    let mut contents = String::new();
+    fs::File::open(path)?.read_to_string(&mut contents)?;
+    
+    let mut sum = 0;
+    for line in contents.lines() {
+        let change : i32 = line.trim().parse()?;
+        sum += change;
+    }
+    println!("Part 1: {}", sum);
+    
+    Ok(())
 }
